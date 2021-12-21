9de895b607893f3a6eb60cb725ab3c52e1bc24d1
AkbarZul <akbar.zul73@gmail.com>
2021-12-21 10:19:12 +0700

    progress home screen

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import Laundry from './images/laundry.png';
+import Dry from './images/dry.png';
+import Coolicon from './images/coolicon.png';
+
+export {Laundry, Dry, Coolicon};
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -7,38 +7,59 @@ import {
   TouchableOpacity,
   ScrollView,
 } from 'react-native';
+import {Laundry, Dry, Coolicon} from '../../assets';
 
 const HomeScreen = () => {
   return (
     <>
-      <View style={styles.container}>
-        <Text style={styles.containerText}>Welcome, John</Text>
-      </View>
-      <View style={styles.subContainer}>
-        <View style={styles.cardBalance}>
-          <View style={styles.containerCircle}>
-            <View style={styles.circle}></View>
-            <View style={styles.circleRed}></View>
-          </View>
-          <View style={styles.containerBalance}>
-            <Text style={styles.containerBalanceText}>Your Balance</Text>
-          </View>
-          <View style={styles.containerPrice}>
-            <Text style={styles.containerPriceText}>$ 200.00</Text>
+      <ScrollView style={{backgroundColor: '#E7F5FD'}}>
+        <View style={styles.container}>
+          <Text style={styles.containerText}>Welcome, John</Text>
+        </View>
+        <View style={styles.subContainer}>
+          <View style={styles.cardBalance}>
+            <View style={styles.containerCircle}>
+              <View style={styles.circle}></View>
+              <View style={styles.circleRed}></View>
+            </View>
+            <View style={styles.containerBalance}>
+              <Text style={styles.containerBalanceText}>Your Balance</Text>
+            </View>
+            <View style={styles.containerPrice}>
+              <Text style={styles.containerPriceText}>$ 200.00</Text>
+            </View>
           </View>
         </View>
-      </View>
-      <View style={styles.containerOrder}>
-        <Text style={styles.orderText}>PREVIOUS ORDER</Text>
-      </View>
-      <View style={styles.laundry}>
+        <View style={styles.containerOrder}>
+          <Text style={styles.orderText}>PREVIOUS ORDER</Text>
+        </View>
+        <View style={styles.laundry}>
           <View style={styles.bagLaundry}>
-
+            <View style={styles.bagLaundryContents}>
+              <View>
+                <Image style={styles.pictureLaundry} source={Laundry} />
+              </View>
+              <View>
+                <Text style={styles.bagLaundryText}>Bag of Laundry</Text>
+                <Text style={styles.laundryTotalText}>Total Order</Text>
+                <Text style={styles.laundryPriceText}>$ 180.00</Text>
+              </View>
+            </View>
           </View>
           <View style={styles.invoice}>
-
+            <Image style={styles.pictureInvoice} source={Coolicon} />
+            <Text style={styles.textInvoice}>INVOICE</Text>
           </View>
-      </View>
+        </View>
+        <View style={styles.containerOrder}>
+          <Text style={styles.mostOrderText}>YOUR MOST ORDERED</Text>
+        </View>
+        <View style={styles.continerDry}>
+          <Image style={styles.pictureDry} source={Dry} />
+        </View>
+        
+      <Text>Test</Text>
+      </ScrollView>
     </>
   );
 };
@@ -47,7 +68,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F36868',
     width: '100%',
-    height: '35%',
+    height: '30%',
     borderBottomLeftRadius: 50,
     borderBottomRightRadius: 50,
     // display: 'flex',
@@ -58,7 +79,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Roboto',
     fontSize: 21,
     fontWeight: '500',
-    marginTop: 100,
+    marginTop: 50,
     color: '#ffffff',
   },
   subContainer: {
@@ -67,7 +88,7 @@ const styles = StyleSheet.create({
     marginTop: -100,
   },
   cardBalance: {
-    backgroundColor: '#ffffff',
+    backgroundColor: '#ccedff',
     width: 341,
     height: 202,
     borderRadius: 10,
@@ -124,24 +145,89 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '700',
     color: '#3b97cb',
-    marginTop: 30
+    marginTop: 30,
   },
   laundry: {
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection: 'row',
-      marginTop: 15,
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
   },
   bagLaundry: {
-      width: 200,
-      height: 95,
-      backgroundColor: '#ffffff'
+    width: 220,
+    height: 95,
+    backgroundColor: '#ffffff',
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
   },
   invoice: {
-      backgroundColor: '#0099ee',
-      width: 132,
-      height: 95,
-  }
+    backgroundColor: '#0099ee',
+    width: 112,
+    height: 95,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pictureInvoice: {
+    width: 44,
+    height: 44,
+  },
+  textInvoice: {
+    fontFamily: 'Roboto',
+    fontSize: 11,
+    fontWeight: '500',
+    color: '#ffffff',
+    marginTop: 10,
+  },
+  bagLaundryContents: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 6,
+    alignItems: 'center',
+  },
+  pictureLaundry: {
+    width: 70,
+    height: 79,
+    marginTop: 10,
+  },
+  bagLaundryText: {
+    fontFamily: 'Roboto',
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#525252',
+  },
+  laundryTotalText: {
+    fontFamily: 'Roboto',
+    fontSize: 10,
+    fontWeight: '400',
+    color: '#535353',
+    marginTop: 15,
+  },
+  laundryPriceText: {
+    fontFamily: 'Roboto',
+    fontSize: 15,
+    fontWeight: '700',
+    color: '#0099ee',
+  },
+  mostOrderText: {
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#3b97cb',
+    marginTop: 30,
+  },
+  continerDry: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingBottom: 10
+  },
+  pictureDry: {
+    width: 320,
+    height: 320,
+  },
 });
 
 export default HomeScreen;
